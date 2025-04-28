cdb2c5d935eae5ad3c38512e5ede617158c8f333
nitishyadav0 <152499116+nitishyadav0@users.noreply.github.com>
2025-04-28 15:37:28 +0530

    Update projet.py

--- projet.py
+++ projet.py
@@ -8,6 +8,10 @@ sns.histplot(df['Employment Rate'], kde=True, color="skyblue")
 plt.title('Histogram of Employment Rate')
 plt.xlabel('Employment Rate')
 plt.ylabel('Frequency')
+
+
+
+
 plt.show()
 sns.barplot(x='Degree', y='Employment Rate', data=df, palette='Set2')
 plt.title('Employment Rate by Degree')
